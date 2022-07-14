c6d0474f148ea23bd72dd46feb06b9b3dbb6b78a
Yehor <yehor.kovalov1@nure.ua>
2022-07-14 11:52:49 +0300

    web

--- DonateChallenger/Web/react-client/src/components/StreamerSearch/StreamerSearch.tsx
+++ DonateChallenger/Web/react-client/src/components/StreamerSearch/StreamerSearch.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react";
-import { useState } from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 import StreamersStore from "../../stores/components/StreamersStore";
 import { useInjection } from "../../utilities/ioc/ioc.react";
 import iocStores from "../../utilities/ioc/iocStores";
@@ -9,31 +9,52 @@ import SearchIcon from "../SearchIcon";
 import './styles.css';
 
 const StreamerSearch = observer(() => {
-     const streamersStore = useInjection<StreamersStore>(iocStores.streamersStore);
 
+     const streamersStore = useInjection<StreamersStore>(iocStores.streamersStore);
      const [showSearchedStreamers, setShowSearchedStreamers] = useState(false);
+
+     useEffect(() => {
+          const handleSearchingStreamersByNickname = async () => {
+               await streamersStore.searchStreamersByNickname();
+          }
+          handleSearchingStreamersByNickname();
+     }, [streamersStore.nicknameForSearching]);
 
      const handleBluring = () => {
           setTimeout(() => {
                setShowSearchedStreamers(false)
           }, 100);
      }
+
      return (
-          <div className="order_search">
-               <MovableInput onBlur={handleBluring} onFocus={() => setShowSearchedStreamers(true)}
-                    value={streamersStore.nicknameForSearching} onChange={e => streamersStore.nicknameForSearching = e.target.value} className="me-3"/>
+          <div className="order_search text-center mt-2 mb-2">
+               <MovableInput placeholder="Streamer..." onBlur={handleBluring} onFocus={() => setShowSearchedStreamers(true)}
+                    value={streamersStore.nicknameForSearching} onChange={e => streamersStore.nicknameForSearching = e.target.value}
+                    className="me-3"/>
                <SearchIcon/>
                {showSearchedStreamers &&
                     <Container className="nicknames_list">
                          {streamersStore.searchedStreamersByNickname.map(s =>
                               <div className="hover_white order_search__nickname"
-                                   role='button' key={s.streamerId} onClick={() =>
-                                        {
-                                             streamersStore.selectStreamer(s)}
-                                        }>
+                                   role='button' key={s.streamerId} onClick={() => streamersStore.selectStreamer(s)}
+                              >
                                         {s.streamerNickname}
-                              </div>)}
+                              </div>)
+                         }
                     </Container>
+               }
+               
+               {streamersStore.selectedStreamer.streamerId &&
+                    <Row className="fs-2 pt-3">
+                         <div className="streamer-parameter_text">
+                              <span>Streamer:</span>
+                              <span className="ms-4 border-bottom">{streamersStore.selectedStreamer.streamerNickname}</span>
+                         </div>
+                         <div className="streamer-parameter_text">
+                              <span>Has minimal donation price:</span>
+                              <span className="ms-4 border-bottom">{streamersStore.selectedStreamer.minDonatePrice}</span>
+                         </div>
+                    </Row>
                }
           </div>
      );
