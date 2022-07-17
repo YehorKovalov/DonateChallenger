96829578de9a0c41451e219fa3d9f4ac184e9efb
Yehor <yehor.kovalov1@nure.ua>
2022-07-18 00:07:00 +0300

    standard extensions for apis' services

--- DonateChallenger/Services/Comment/Comment.API/Program.cs
+++ DonateChallenger/Services/Comment/Comment.API/Program.cs
@@ -1,18 +1,32 @@
+using Comment.API.Data;
 using Comment.API.Extensions;
+using Infrastructure.Extensions;
 using Infrastructure.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 builder.Services
-    .AddDbContexts(builder.Configuration)
+    .AddDbContexts(configuration)
+    .AddCustomAuthorization(configuration)
+    .AddCustomConfiguredSwagger("Comment", configuration, Scopes())
     .AddControllers(o => o.Filters.Add(typeof(HttpGlobalExceptionFilter)))
     .AddJsonOptions(o => o.JsonSerializerOptions.WriteIndented = true);
 
 var app = builder.Build();
+
+app.UseCustomConfiguredSwaggerWithUI(configuration, "Comment", "commentswaggerui");
+
+app.UseRouting();
 
 app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.CreateDbIfNotExist(new AppDbInitializer());
 app.Run();
+
+Dictionary<string, string> Scopes() => new Dictionary<string, string>
+{
+    { "comment", "comment" }
+};
