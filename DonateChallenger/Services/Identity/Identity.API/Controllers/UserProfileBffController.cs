b3506f1394b103c7385e83ebaaaf26db672c15b5
Yehor <yehor.kovalov1@nure.ua>
2022-07-20 16:26:19 +0300

    UserProfile controller + dto

---
+++ DonateChallenger/Services/Identity/Identity.API/Controllers/UserProfileBffController.cs
@@ -0,0 +1,32 @@
+using Identity.API.Models.DTOs;
+using Identity.API.Models.Responses;
+using Identity.API.Services.Abstractions;
+using Infrastructure;
+
+namespace Identity.API.Controllers;
+
+[SecurityHeaders]
+[ApiController]
+[Route(Defaults.DefaultRoute)]
+public class UserProfileBffController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserProfileBffController(IUserService userService) =>_userService = userService;
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(ChangeProfileDataResponse<string>), (int) HttpStatusCode.OK)]
+    public async Task<IActionResult> ChangeNickname(string userId, string newNickname)
+    {
+        var result = await _userService.ChangeUserNickname(userId, newNickname);
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(GetUserProfileResponse<UserProfileDto>), (int) HttpStatusCode.OK)]
+    public async Task<IActionResult> UserProfile(string userId)
+    {
+        var result = await _userService.GetUserProfile(userId);
+        return Ok(result);
+    }
+}
---
+++ DonateChallenger/Services/Identity/Identity.API/Models/DTOs/UserProfileDto.cs
@@ -0,0 +1,7 @@
+namespace Identity.API.Models.DTOs;
+
+public class UserProfileDto
+{
+    public string UserId { get; set; } = null!;
+    public string UserNickname { get; set; } = null!;
+}
