f347334a50a21f717b8895e289a8d0373329ca28
Yehor <yehor.kovalov1@nure.ua>
2022-07-20 16:55:36 +0300

    stores refactored

--- DonateChallenger/Web/react-client/src/stores/states/CommentsStore.ts
+++ DonateChallenger/Web/react-client/src/stores/states/CommentsStore.ts
@@ -21,18 +21,20 @@ export default class CommentsStore {
      paginatedComments: PaginatedComments<Comment> | null = null;
 
      public getChallengeComments = async (currentPage: number, commentsPerPage: number, challengeId: number) => {
+          console.log(`get comments ${challengeId}`)
 
-          const paginatedChallenges = await this.commentService.getPaginatedComments(currentPage, commentsPerPage, challengeId);
-          this.paginatedComments = paginatedChallenges;
+          const paginatedComments = await this.commentService.getPaginatedComments(currentPage, commentsPerPage, challengeId);
+          this.paginatedComments = paginatedComments
      }
 
      public getUserCommentsForChallenge = async (currentPage: number, commentsPerPage: number, challengeId: number, userId: string) => {
 
           if (!this.authStore.user || !(this.authStore.userRole == UserRole.Manager)) {
+               alert("You don't have an access")
                return;
           }
 
-          const paginatedChallenges = await this.commentService.getUserPaginatedComments(currentPage, commentsPerPage, challengeId, userId);
-          this.paginatedComments = paginatedChallenges;
+          const paginatedComments = await this.commentService.getUserPaginatedComments(currentPage, commentsPerPage, challengeId, userId);
+          this.paginatedComments = paginatedComments;
      }
 }
