4e44058391a230057edb9d3115f8faf42074dc49
Yehor <yehor.kovalov1@nure.ua>
2022-07-20 16:48:17 +0300

    datetimestore is added

--- DonateChallenger/Web/react-client/src/components/CurrentChallengeCard/CurrentChallengeCard.tsx
+++ DonateChallenger/Web/react-client/src/components/CurrentChallengeCard/CurrentChallengeCard.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react';
 import { Card, Col, Row, Stack } from 'react-bootstrap';
+import DateTimeStore from '../../stores/components/DateTimeStore';
 import ChallengeStore from '../../stores/states/ChallengeStore';
 import { useInjection } from '../../utilities/ioc/ioc.react';
 import iocStores from '../../utilities/ioc/iocStores';
@@ -7,7 +8,7 @@ import ButtonWithMovableBorder from '../ButtonWithMovableBorder';
 import ButtonWithShadowAndEffectOnHover from '../ButtonWithShadowAndEffectOnHover';
 import './styles.css';
 
-export interface ChallengeCardProps {
+export interface CurrentChallengeCardProps {
      challengeId: number,
      title?: string,
      description: string,
@@ -16,17 +17,18 @@ export interface ChallengeCardProps {
      createdTime: string
 }
 
-const CurrentChallengeCard = observer((props: ChallengeCardProps) => {
+const CurrentChallengeCard = observer((props: CurrentChallengeCardProps) => {
 
-     const store = useInjection<ChallengeStore>(iocStores.challengeStore);
-     const className = props.challengeId === store.lastUsedChallengeId ? 'color-silver border  blur' : 'color-silver border';
+     const challengeStore = useInjection<ChallengeStore>(iocStores.challengeStore);
+     const dateTimeStore = useInjection<DateTimeStore>(iocStores.dateTimeStore);
+     const className = props.challengeId === challengeStore.lastUsedChallengeId ? 'color-silver border  blur' : 'color-silver border';
 
      return (
           <Card bg='dark' key={props.challengeId} className={className}>
                <Card.Body>
                     <Stack direction='horizontal'>
                          <div className='donate-donater'>{props.donateFrom}</div>
-                         <div className='ms-auto'>{store.getUserFriendlyDateTime(props.createdTime)}</div>
+                         <div className='ms-auto'>{dateTimeStore.getUserFriendlyDateTime(props.createdTime)}</div>
                     </Stack>
                     <div className='donate-price'>{props.donatePrice}$</div>
                     <div><span className='color-silver'>{props.title}</span></div>
@@ -37,11 +39,11 @@ const CurrentChallengeCard = observer((props: ChallengeCardProps) => {
                     <Row className='mt-3 ms-4'>
                          <Col lg={8}>
                               <ButtonWithMovableBorder title='Completed'
-                                   onClick={async () => await store.completeChallenge(props.challengeId)}/>
+                                   onClick={async () => await challengeStore.completeChallenge(props.challengeId)}/>
                          </Col>
                          <Col>
                          <ButtonWithShadowAndEffectOnHover title='Skip'
-                              onClick={async () => await store.skipChallenge(props.challengeId)}/>
+                              onClick={async () => await challengeStore.skipChallenge(props.challengeId)}/>
                          </Col>
                     </Row>
                </Card.Body>
