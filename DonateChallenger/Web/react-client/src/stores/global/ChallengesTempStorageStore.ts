43034b187c03861de88a1c7dafc748bfa1e844fa
Yehor <yehor.kovalov1@nure.ua>
2022-07-22 00:14:05 +0300

    auth store/service + components updating with roles

--- DonateChallenger/Web/react-client/src/stores/global/ChallengesTempStorageStore.ts
+++ DonateChallenger/Web/react-client/src/stores/global/ChallengesTempStorageStore.ts
@@ -1,9 +1,13 @@
 import { inject, injectable } from "inversify";
 import { makeAutoObservable } from "mobx";
 import { ChallengeForAddingDto } from "../../dtos/DTOs/ChallengeForAddingDto";
+import { UserRole } from "../../models/UserRole";
+import AuthStore from "../../oidc/AuthStore";
 import { ChallengesTempStorageService } from "../../services/ChallengesTempStorageService";
 import iocServices from "../../utilities/ioc/iocServices";
 import iocStores from "../../utilities/ioc/iocStores";
+import StreamerProfileStore from "../containers/StreamerProfileStore";
+import UserProfileStore from "../containers/UserProfileStore";
 import ChallengeForAddingStore from "../states/ChallengeForAddingStore";
 
 @injectable()
@@ -11,6 +15,9 @@ export default class ChallengesTempStorageStore {
 
      @inject(iocServices.challengesTempStorageService) private readonly challengesTempStorageService!: ChallengesTempStorageService;
      @inject(iocStores.challengeForAddingStore) private readonly challengeForAddingStore!: ChallengeForAddingStore;
+     @inject(iocStores.userProfileStore) private readonly userProfile!: UserProfileStore;
+     @inject(iocStores.streamerProfileStore) private readonly streamerProfileStore!: StreamerProfileStore;
+     @inject(iocStores.authStore) private readonly authStore!: AuthStore;
 
      constructor() {
           makeAutoObservable(this);
@@ -30,6 +37,7 @@ export default class ChallengesTempStorageStore {
 
      public addChallengeToStorage = async (streamerId: string): Promise<boolean> => {
 
+          await this.handleUserRole();
           if (!this.challengeForAddingStateIsValid(streamerId)) {
                return false;
           }
@@ -70,32 +78,47 @@ export default class ChallengesTempStorageStore {
           const logicalPriceMinimum = 0.1;
           const descriptionStateIsValid = this.challengeForAddingStore.description.state.length > 0;
           if (!descriptionStateIsValid) {
-               console.log("description");
+               console.log("description is not valid");
                console.log(this.challengeForAddingStore.description.state);
                return false;
           }
           const streamerIdStateIsValid = streamerId.length > 0;
 
-
           if (!streamerIdStateIsValid) {
-               console.log("streamerId");
+               console.log("streamerId is not valid");
                return false;
           }
 
-          const donateFromStateIsValid = this.challengeForAddingStore.donateFrom.state.length > 0;
+          const donateFromStateIsValid = this.challengeForAddingStore.donateFrom?.state?.length > 0;
 
           if (!donateFromStateIsValid) {
-               console.log("donate");
+               console.log("donate is not valid");
                return false;
           }
 
-          const donatePriceStateIsValid = this.challengeForAddingStore.donatePrice.state > logicalPriceMinimum;
+          const donatePriceStateIsValid = this.challengeForAddingStore.donatePrice?.state > logicalPriceMinimum;
 
           if (!donatePriceStateIsValid) {
-               console.log("donatePrice");
+               console.log("donatePrice is not valid");
                return false;
           }
 
           return true;
+     }
+
+     private handleUserRole = async () => {
+          switch (this.authStore.userRole) {
+               case UserRole.Anonymous:
+                    this.challengeForAddingStore.donateFrom.state = "Anonymous"
+                    break;
+               case UserRole.Donater:
+                    await this.userProfile.getUserProfile();
+                    this.challengeForAddingStore.donateFrom.state = this.userProfile.profile.userNickname;
+                    break;
+               case UserRole.Streamer:
+                    await this.streamerProfileStore.getStreamerProfile();
+                    this.challengeForAddingStore.donateFrom.state = this.streamerProfileStore.profile.streamerNickname
+                    break;
+          }
      }
 }
